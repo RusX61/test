adeba36e129d884f042166360e44a841dce6a754
rusx61 <rusx.lan@gmail.com>
2022-06-05 18:45:14 +0300

    add vcoy

---
+++ index.js
@@ -0,0 +1,12 @@
+const Vcoy = {
+FirstName: 'Vitya',
+LastName: 'Coy',
+Gender: 'Male',
+Age: 28,
+HP:null,
+Location:undefined,
+BloodGroup:'На рукаве',
+IsAlive: true
+};
+
+console.log(BloodGroup);
