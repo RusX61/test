bd34929dce35c85e087407f046d59d6202c2dc65
rusx61 <rusx.lan@gmail.com>
2022-06-11 16:30:42 +0300

    add functions

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const Vcoy = {
-    firstName: 'Vitya',
-    lastName: 'Coy',
-    gender: 'Male',
-    age: 28,
-    hp:null,
-    location:undefined,
-    bloodGroup:'На рукаве',
-    isAlive: true
-};
+function sum(a,b){
+    let result = a+b;
+    console.log(result);
+}
+sum(4,66);
 
-console.log(Vcoy.IsAlive);
+function x(a,b) {
+    return a * b;
+}
+const result = x(9,7);
+console.log(result);
+
+function alrt(){
+    alert ('Сказано же было не нажимать,долбоеб!')
+}
+
+const button = document.querySelector('button');
+button.addEventListener ('click', alrt);
+button.addEventListener ('click', sum);
---
+++ vcoy.js
@@ -0,0 +1,12 @@
+const Vcoy = {
+    firstName: 'Vitya',
+    lastName: 'Coy',
+    gender: 'Male',
+    age: 28,
+    hp:null,
+    location:undefined,
+    bloodGroup:'На рукаве',
+    isAlive: true
+};
+
+console.log(Vcoy.isAlive);
