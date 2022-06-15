e3f8a12775ab35be8b7df5563d58caa771d4a349
rusx61 <rusx.lan@gmail.com>
2022-06-15 23:03:17 +0300

    update repo

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function sum(a,b){
-    let result = a+b;
-    console.log(result);
-}
-sum(4,66);
+// //Функции
+// function sum(a,b){
+//     let result = a+b;
+//     console.log(result);
+// }
+//sum(4,66);
 
-function x(a,b) {
-    return a * b;
-}
-const result = x(9,7);
-console.log(result);
+// function x(a,b) {
+//     return a * b;
+// }
+// const result = x(9,7);
+// console.log(result);
 
-function alrt(){
-    alert ('Сказано же было не нажимать,долбоеб!')
-}
+// function alrt(){
+//     alert ('Сказано же было не нажимать,долбоеб!')
+// }
 
-const button = document.querySelector('button');
-button.addEventListener ('click', alrt);
-button.addEventListener ('click', sum);
+// const button = document.querySelector('button');
+// button.addEventListener ('click', alrt);
+// button.addEventListener ('click', sum);
+
+
+// //Объект
+// const Vcoy = {
+//     firstName: 'Vitya',
+//     lastName: 'Coy',
+//     gender: 'Male',
+//     age: 28,
+//     hp:null,
+//     location:undefined,
+//     bloodGroup:'На рукаве',
+//     isAlive: true
+// };
+
+//console.log(Vcoy.isAlive);
