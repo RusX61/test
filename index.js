1e12ee8b66fcbaae350b7fac74439eec6cb61807
rusx61 <rusx.lan@gmail.com>
2022-06-21 20:18:05 +0300

    логические операторы/функция авторизации

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-//Сравнения
-const comparisonInt = 5>4;
-console.log(comparisonInt);
+// const car = undefined; // если есть символы - то true, если пустая строка - false
+// const day = 0; //если есть число - то true, если будет 0 - false
+// const something = undefined; // false
+// const notning = null; //false
 
-const comparisonStr = "2" > "12";
-console.log(comparisonStr);
+// const result1 = day || car; // ИЛИ возвращает первое true
+// const result2 = day && car; //И если все значения true - веренет последнее, если какое то значение false - то вернет предыдущее или его(если оно первое)
+// const result3 = !day; // отрицание
+// const result4 = day ?? car; // возвращает первое значение которое не равно null или undefined
 
-const comparisonUndefinedNull = undefined == null;
-console.log(comparisonUndefinedNull);
+const acount = {
+    login: null,
+    password: null,
+    phone: null,
+    admin: fale,
+}
 
-const comparisonUndefinedNullBulean = undefined === null;
-console.log(comparisonUndefinedNullBulean);
+function getAction() {
+    if (!acount.login && !acount.admin) {
+        console.log('Пожалуйста введите логин!')
+    } else if (!acount.password) {
+        console.log('Пожалуйста введите пароль!')
+    } else if (!acount.phone) {
+        console.log('Пожалуйста введите телефон!')
+    } else {
+        console.log('Добро пожаловать!')
+    }
+}
 
-const comparisonNullStr = null == "\n0\n";
-console.log(comparisonNullStr);
+acount['login'] = 'developer';
+acount['password'] = '1234';
+acount['phone'] = '7999999999';
 
-const comparisonNullStrBulean = null === +"\n0\n";
-console.log(comparisonNullStrBulean);
-
-//Условия
-let timesOfDay="ночь";
-let wife=true;
-if (timesOfDay=="утро" & wife==true){
-        result="Поебись и позавтракай";
-    }else if (timesOfDay=="утро" & wife==false){
-        result="Подрочи и позавтракай";
-    }else if (timesOfDay=="день"){
-        result="Потрень и пообедай";
-    } else if (timesOfDay=="вечер"){
-        result="Поужинай и отдохни";
-    } else result="Спи!";
-console.log(result);
+getAction()
