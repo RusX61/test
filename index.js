37b2f40a837f6ae320973032ba43eeefa0777d3e
rusx61 <rusx.lan@gmail.com>
2022-06-22 21:04:08 +0300

    расчет ипотеки

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-// const car = undefined; // если есть символы - то true, если пустая строка - false
-// const day = 0; //если есть число - то true, если будет 0 - false
-// const something = undefined; // false
-// const notning = null; //false
+const basicPercent = 15; //базовый процент
+const borrowers = ["семейная ипотека","льготная ипотека","обычная","ипотека для IT-специалистов"]; //набор ипотек
+const discounts = [8, 13, 5.5] //скидки
 
-// const result1 = day || car; // ИЛИ возвращает первое true
-// const result2 = day && car; //И если все значения true - веренет последнее, если какое то значение false - то вернет предыдущее или его(если оно первое)
-// const result3 = !day; // отрицание
-// const result4 = day ?? car; // возвращает первое значение которое не равно null или undefined
-
-const acount = {
-    login: null,
-    password: null,
-    phone: null,
-    admin: fale,
-}
-
-function getAction() {
-    if (!acount.login && !acount.admin) {
-        console.log('Пожалуйста введите логин!')
-    } else if (!acount.password) {
-        console.log('Пожалуйста введите пароль!')
-    } else if (!acount.phone) {
-        console.log('Пожалуйста введите телефон!')
-    } else {
-        console.log('Добро пожаловать!')
+for(let i=0; i< borrowers.length; i+=1){
+    if (i==0){
+        totalPercent = basicPercent - discounts[0];
+        console.log(borrowers[i] + ' : ' + totalPercent + '%');
+    } else if (i==1){
+        totalPercent = basicPercent - discounts[1];
+        console.log(borrowers[i] + ' : ' +totalPercent + '%');
+    } else if (i==2) {console.log(borrowers[i] + ' : ' + basicPercent + '%')
+    } else if (i==3){
+        totalPercent = basicPercent - discounts[2];
+        console.log(borrowers[i] + ' : ' + totalPercent + '%');
     }
 }
-
-acount['login'] = 'developer';
-acount['password'] = '1234';
-acount['phone'] = '7999999999';
-
-getAction()
