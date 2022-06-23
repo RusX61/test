bff2f7311b50671ad6c20092758d0f004f3c1cb0
rusx61 <rusx.lan@gmail.com>
2022-06-23 13:05:45 +0300

    добавление альтернативого решения расчета ипотеки

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+// const basicPercent = 15; //базовый процент
+// const borrowers = ["семейная ипотека","льготная ипотека","обычная","ипотека для IT-специалистов"]; //набор ипотек
+// const discounts = [8, 13, 5.5] //скидки
+
+// for(let i=0; i< borrowers.length; i+=1){
+//     if (i==0){
+//         totalPercent = basicPercent - discounts[0];
+//         console.log(borrowers[i] + ' : ' + totalPercent + '%');
+//     } else if (i==1){
+//         totalPercent = basicPercent - discounts[1];
+//         console.log(borrowers[i] + ' : ' +totalPercent + '%');
+//     } else if (i==2) {console.log(borrowers[i] + ' : ' + basicPercent + '%')
+//     } else if (i==3){
+//         totalPercent = basicPercent - discounts[2];
+//         console.log(borrowers[i] + ' : ' + totalPercent + '%');
+//     }
+// }
+
+
+
 const basicPercent = 15; //базовый процент
-const borrowers = ["семейная ипотека","льготная ипотека","обычная","ипотека для IT-специалистов"]; //набор ипотек
+const borrowers = ["семейная ипотека","льготная ипотека","ипотека для IT-специалистов","обычная"]; //набор ипотек
 const discounts = [8, 13, 5.5] //скидки
 
 for(let i=0; i< borrowers.length; i+=1){
-    if (i==0){
-        totalPercent = basicPercent - discounts[0];
-        console.log(borrowers[i] + ' : ' + totalPercent + '%');
-    } else if (i==1){
-        totalPercent = basicPercent - discounts[1];
-        console.log(borrowers[i] + ' : ' +totalPercent + '%');
-    } else if (i==2) {console.log(borrowers[i] + ' : ' + basicPercent + '%')
-    } else if (i==3){
-        totalPercent = basicPercent - discounts[2];
-        console.log(borrowers[i] + ' : ' + totalPercent + '%');
-    }
+    if(i<3){
+    totalPercent = basicPercent - discounts[i];
+    console.log(borrowers[i] + ' : ' + totalPercent + '%');
+    }else console.log(borrowers[i] + ' : ' + basicPercent + '%');
 }
