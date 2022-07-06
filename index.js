8936f8f9a4268c225ddbd480889919f8eaed5908
Ruslan Merzlyakov <rusx.lan@gmail.com>
2022-07-06 20:46:00 +0300

    IMT bug fix

--- index.js
+++ index.js
@@ -26,9 +26,17 @@ submitButton.onclick = function() {
     let height = document.getElementById('userHeight').value;
     let age = document.getElementById('userAge').value;
     let gender = document.querySelector('input[name = "gender"]:checked').value;
-    let result ;
-    if (gender = male){
-        result = (height - 100)*1.15;
-    } else result = (height - 110)*1.15;
-    alert(result);
+    let imt = weight/((height/100)**2);
+    let result;
+    if (gender == 'male') {
+        result = (height - 100);;
+    } else result = (height - 110);
+
+    if(age < 30){
+        result=result-((result/100)*11);
+    }else if (age > 50){
+        result=result+((result/100)*6);
+    }
+
+    alert(result + "Индекс Массы вашего тела =" + imt);
 }
