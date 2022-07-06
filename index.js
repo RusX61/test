a0da4e2cf501d91dc49c629dedc12fb8a591ae0e
Ruslan Merzlyakov <rusx.lan@gmail.com>
2022-07-06 20:47:30 +0300

    IMT bug fix

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-
-// for(let i=0; i< borrowers.length; i+=1){
-//     if (i==0){
-//         totalPercent = basicPercent - discounts[0];
-//         console.log(borrowers[i] + ' : ' + totalPercent + '%');
-//     } else if (i==1){
-//         totalPercent = basicPercent - discounts[1];
-//         console.log(borrowers[i] + ' : ' +totalPercent + '%');
-//     } else if (i==2) {console.log(borrowers[i] + ' : ' + basicPercent + '%')
-//     } else if (i==3){
-//         totalPercent = basicPercent - discounts[2];
-//         console.log(borrowers[i] + ' : ' + totalPercent + '%');
-//     }
-// }
-
-
-// for(let i=0; i< borrowers.length; i+=1){
-//     if(i<3){
-//     totalPercent = basicPercent - discounts[i];
-//     console.log(borrowers[i] + ' : ' + totalPercent + '%');
-//     }else console.log(borrowers[i] + ' : ' + basicPercent + '%');
-// }
-
 submitButton.onclick = function() {
     let weight = document.getElementById("userWeight").value;
     let height = document.getElementById('userHeight').value;
