301d32eb3e57b5ebe1eb8c5b238d81523ff021ad
Ruslan Merzlyakov <rusx.lan@gmail.com>
2022-07-12 15:45:45 +0300

    beta

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ submitButton.onclick = function() {
     alert("Индекс Массы вашего тела = " + imt + "\x0A" + 
     'Ваш нормальный вес: '+ result + 'КГ' + '\x0A' + recomendation);
 }
+
+var img = document.querySelectorAll('.click'),
+  backPop = document.getElementById('backPop'),
+    popUp = document.getElementById('popUp'),
+    bigImg= document.getElementById('bigImg');
+[].forEach.call(img, function(elem) {
+  var src = elem.getAttribute('src');
+  elem.onclick = function() { 
+    bigImg.setAttribute('src',src);
+    backPop.style.display = "block";
+  }
+});
+backPop.onclick = function(elem){
+  if(!(elem.target.closest("#popUp"))){
+      backPop.style.display = 'none';
+     }    
+}
